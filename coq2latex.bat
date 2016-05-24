db7b59906f3b52356fc102fb5a01fc214e26fc1f
Johannes <jogus@live.de>
2016-05-24 17:17:07 -0400

    split scripts

---
+++ coq2latex.bat
@@ -0,0 +1,3 @@
+@echo off
+REM === Usage: coq2latex.bat <coq-file (without .v extension)> <name of inductive proposition>
+((echo Set Printing Depth 2. Load %1. Set Printing All. Set Printing Width 2097151. Set Printing Depth 100. Print %2. Quit. | coqtop.exe) && type %1.v) | %~dp0\project\coq2latex\bin\Debug\coq2latex.exe %2
