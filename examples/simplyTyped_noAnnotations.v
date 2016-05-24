e5f1a684651a935360d61b24651a2c975f8273ab
Johannes <jogus@live.de>
2016-05-24 17:34:16 -0400

    doc

--- examples/simplyTyped_noAnnotations.v
+++ examples/simplyTyped_noAnnotations.v
@@ -1,39 +1,28 @@
 Require Import Coq.Unicode.Utf8 Arith Bool Ring Setoid String.
 
 Inductive SimpleType : Set :=
-(*coq2latex: TInt := \texttt{int} *)
 | TInt : SimpleType
-(*coq2latex: TBool := \texttt{bool} *)
 | TBool : SimpleType
-(*coq2latex: TFunc #a #b := #a \rightarrow #b *)
 | TFunc : SimpleType -> SimpleType -> SimpleType
 .
 
 Inductive SimpleValue : Set :=
-(*coq2latex: VNum #n := #n *)
 | VNum : nat -> SimpleValue
-(*coq2latex: VTrue := \texttt{true} *)
 | VTrue : SimpleValue
-(*coq2latex: VFalse := \texttt{false} *)
 | VFalse : SimpleValue
 .
 
 Definition SimpleVariable := string.
 
 Inductive SimpleExpression : Set :=
-(*coq2latex: EValue #v := #v *)
 | EValue : SimpleValue -> SimpleExpression
-(*coq2latex: EVariable #x := #x *)
 | EVariable : SimpleVariable -> SimpleExpression
-(*coq2latex: EApplication #a #b := #a~#b *)
 | EApplication : SimpleExpression -> SimpleExpression -> SimpleExpression
-(*coq2latex: ELambda #x #t #e := (\lambda #x : #t . #e) *)
 | ELambda : SimpleVariable -> SimpleType -> SimpleExpression -> SimpleExpression
 .
 
 Definition SimpleEnvironment := SimpleVariable -> option SimpleType.
 
-(*coq2latex: SimpleEnvironmentSet #G #x #t := #G, #x : #t *)
 Definition SimpleEnvironmentSet
               (env : SimpleEnvironment)
               (x : SimpleVariable)
@@ -43,24 +32,18 @@ Definition SimpleEnvironmentSet
                             then Some t
                             else env x.
 
-(*coq2latex: SimpleExpressionHasType #G #e #t := #G \vdash #e : #t *)
 Inductive SimpleExpressionHasType : SimpleEnvironment -> SimpleExpression -> SimpleType -> Prop :=
-| TyNum : forall n env(*\Gamma*), SimpleExpressionHasType env (EValue (VNum n)) TInt
-| TyTrue  : forall env(*\Gamma*), SimpleExpressionHasType env (EValue VTrue) TBool
-| TyFalse : forall env(*\Gamma*), SimpleExpressionHasType env (EValue VFalse) TBool
-| TyVariable : forall env(*\Gamma*) tau(*\*) x,
+| TyNum : forall n env, SimpleExpressionHasType env (EValue (VNum n)) TInt
+| TyTrue  : forall env, SimpleExpressionHasType env (EValue VTrue) TBool
+| TyFalse : forall env, SimpleExpressionHasType env (EValue VFalse) TBool
+| TyVariable : forall env tau x,
     env x = Some tau ->
     SimpleExpressionHasType env (EVariable x) tau
-| TyApplication : forall env(*\Gamma*) e_0 e_1 tau(*\*) tau'(*\*),
-    SimpleExpressionHasType env e_0 (TFunc tau(*\*) tau'(*\*)) ->
+| TyApplication : forall env e_0 e_1 tau tau',
+    SimpleExpressionHasType env e_0 (TFunc tau tau') ->
     SimpleExpressionHasType env e_1 tau ->
     SimpleExpressionHasType env (EApplication e_0 e_1) tau'
-| TyLambda : forall env(*\Gamma*) x e tau(*\*) tau'(*\*),
+| TyLambda : forall env x e tau tau',
     SimpleExpressionHasType (SimpleEnvironmentSet env x tau) e tau' ->
     SimpleExpressionHasType env (ELambda x tau e) (TFunc tau tau')
 .
-
-
-(*coq2latex: @Some #_ #x := #x *)
-(*coq2latex: @eq #_ #a #b := #a = #b *)
-
